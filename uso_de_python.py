0fa454ee18fb2407dd0d3f770e28dbefb1007d8f
Jhofre2102 <jhonospinaduran@gmail.com>
2025-03-14 19:18:33 -0500

    introduccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,38 @@
+# Autor :Jhon Ospina
+# Este en un cometario de una sola linea
+'''
+Este es un comenatrio
+multiples
+lineas
+'''
+print("Estudiando python")#
+#definicion de variables
+'''
+Es sensible a minusculas y mayusculasno 
+No se puden usar espacios
+No se pueden usuar palabras reservadas
+No es conveniente usuar como nombre de variables
+
+'''
+x=50
+y=15
+print(x+y)
+
+
+
+print(type (x))
+#<class 'int'>: significa que x es un numero entero
+
+nombre="Jhon"
+print(nombre)
+print(type(nombre))
+#<class 'str'> : la variable nombre es un string (cadena)
+
+print(type (x))
+x=4.8
+print(type (x))
+#<class 'float'> significa que la variable es un flotante (nuemro decimal)
+
+print (x,nombre)
+
+
