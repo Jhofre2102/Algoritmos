1f3017e14a3f67d1455dd63a6f0f3182d0891bfd
Jhofre2102 <jhonospinaduran@gmail.com>
2025-03-19 19:31:40 -0500

    tipos de operadores en python

---
+++ tipos_de_operadores.py
@@ -0,0 +1,135 @@
+'''
+Operadores Aritmeticos
+
+'''
+
+# # suma
+# x=2+6
+# print(x)
+
+# # Resta
+# y=8-2
+# print(y)
+
+
+# # Multiplicacion 
+# print(2*25)
+
+
+# # Division
+# ''' para python la division simepre retorna un float'''
+# divide=25/5
+# print(divide)
+#print(type(divide))
+
+
+#Division piso  (floor)
+# ''' la division piso me devuelve el numero entero mas proximo hacia abajo'''
+
+# print(10/3)
+# print(10//3)
+
+
+# # print(14.5//3)
+
+# ''' Prueba realizar la division piso 8//-3 y comparala con la division tradicional 8/-3'''
+# print(8//-3)
+#print(8/-3)
+
+
+#Modulo
+
+# w=20/3
+# y=20%3
+# print(w)
+# print(y)
+
+# print(type(y))
+
+#Potencia
+
+print(2**3)
+
+
+'''
+
+Operadores de Comparacion
+
+Este  tipo  de  operadores  los  usamos  cuando  deseamos  comparar  expresiones  o variables. 
+Python evalúa si se  cumple la comparación y nos devuelve (retorna) un valor True o False 
+
+ '''
+
+#Es igual
+
+# print("x"=="X")
+# print(3==3.0)
+#print(3==3.00000000000000000001)
+
+
+# es difernte a
+
+print(4!=3)
+print(False !=True)
+
+#es mayor que
+#print(4>8)
+#
+
+''' 
+
+Operadores de asigancion
+
+'''
+
+#Incremento
+
+x=20
+x=20+1
+
+#print(x)
+
+#incremento
+#+=
+#print(x)
+
+#decrecimiento
+
+#y=5
+#y=y-1
+#y-=1
+#print(y)
+
+
+#Asigna Multiplicacion
+
+''' 
+
+Operadores Logicos
+and exige que todas las condiciones sean true para responder true
+or: este operador indica que algunas de las condiciones sea true
+not
+
+'''
+x=4 #defino el valor
+#x==4 #preguntando si x es igual a acompara
+y=4
+#print(x==4 and y==4)
+#uso del oprador or
+
+print(x==100 or y==4)
+
+#uso del operador not
+'''
+se valida si una variable es falsa o true.
+si la variable existe en memoria tenemos un valor por defecto
+
+
+
+'''
+#print(not x)
+
+#enunciado=false
+#print(not enunciado)
+
+usuario_logueado=True
